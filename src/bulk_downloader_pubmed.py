c166af8a31bca0f9d3633eb83be01f7d37eb033b
rafsanahmed <rafsan.ahmed@std.antalya.edu.tr>
2023-06-07 11:33:31 +0200

    updates

--- src/bulk_downloader_pubmed.py
+++ src/bulk_downloader_pubmed.py
@@ -7,10 +7,13 @@ from tqdm import tqdm, trange
 
 if __name__ == "__main__":
 
-    save_path = "../data/tmp/"
+    save_path = "../data/tmp/update_files/"
+    os.makedirs(save_path, exists_ok=True)
+    
     f = open(f"{save_path}err.txt", "w", encoding="utf8")
-    for i in trange(1,1767):
-        url = f'https://data.lhncbc.nlm.nih.gov/public/ii/information/MBR/Baselines/2023/pubmed23n{i:04d}.xml.gz'
+    for i in trange(1167,1371):
+        # url = f'https://data.lhncbc.nlm.nih.gov/public/ii/information/MBR/Baselines/2023/pubmed23n{i:04d}.xml.gz'
+        url = f'https://ftp.ncbi.nlm.nih.gov/pubmed/updatefiles/pubmed23n{i:04d}.xml.gz'
         try:
             urllib.request.urlretrieve(url, filename=f"{save_path}pubmed23n{i:04d}.xml.gz")
         except:
